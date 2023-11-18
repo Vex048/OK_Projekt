76a4f02085aaecf3c254736d834edc7e5e6ad656
smazik02 <s.mazik2002@gmail.com>
2023-11-18 18:13:22 +0100

    small fix

--- instances.py
+++ instances.py
@@ -11,7 +11,7 @@ def randomInstance(instanceSize, boardX, boardY):
     edges = []
     x = [int(x) for x in range(boardX-1)]
     y = [int(x) for x in range(boardY-1)]
-    i = 0
+    i = 1
     while len(edges) != instanceSize:
         point = [i, random.choice(x), random.choice(y)]
         if point not in edges:
