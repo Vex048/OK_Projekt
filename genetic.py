4ff42bbe412832d7e14273b98466be79c8190406
smazik02 <s.mazik2002@gmail.com>
2023-12-05 13:39:39 +0100

    fixed most issues

--- genetic.py
+++ genetic.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import random
 import sys
 import math
@@ -13,8 +14,8 @@ class Individual(object):  # Klasa osobników
 
     @staticmethod
     def mate(parent1, parent2):
-        geneA = random.randint(0, pointCount-1)
-        geneB = random.randint(0, pointCount-1)
+        geneA = random.randint(1, pointCount-1)
+        geneB = random.randint(1, pointCount-1)
         start = min(geneA, geneB)
         end = max(start+1, geneA, geneB)
 
@@ -25,24 +26,37 @@ class Individual(object):  # Klasa osobników
         #     if x not in tmp:
         #         remaining.append(x)
         res = remaining[:start] + tmp + remaining[start:]
-
+        if len(res) != pointCount+1:
+            res.append(res[0])
         return res
 
-    def mutate(self, child):
+    def mutate(self):
         while True:
-            geneA = random.randint(0, pointCount-1)
-            geneB = random.randint(0, pointCount-1)
+            geneA = random.randint(1, pointCount-1)
+            geneB = random.randint(1, pointCount-1)
             if geneA != geneB:
-                child[geneA], child[geneB] = child[geneB], child[geneA]
-                break
-        return child
+                # oldDistance = self.distanceBetweenGenes(geneA-1, geneB+1)
+                self.order[geneA], self.order[geneB] = self.order[geneB], self.order[geneA]
+                # newDistance = self.distanceBetweenGenes(geneA-1, geneB+1)
+                # self.distance -= oldDistance
+                # self.distance += newDistance
+                # print(f"Mutate, oldDistance {oldDistance}, newDistance {newDistance}")
+                return
 
     def calDistance(self):
         totalDistance = 0
         startPoint = self.order[0]
         for endPoint in self.order[1:]:
-            distance = math.sqrt(
-                pow((endPoint[1]-startPoint[1]), 2) + pow((endPoint[2]-startPoint[2]), 2))
+            distance = math.sqrt(pow((endPoint[1]-startPoint[1]), 2) + pow((endPoint[2]-startPoint[2]), 2))
+            totalDistance += distance
+            startPoint = endPoint
+        return totalDistance
+    
+    def distanceBetweenGenes(self, geneA, geneB):
+        totalDistance = 0
+        startPoint = self.order[geneA]
+        for endPoint in self.order[geneA+1:geneB+1]:
+            distance = math.sqrt(pow((endPoint[1]-startPoint[1]), 2) + pow((endPoint[2]-startPoint[2]), 2))
             totalDistance += distance
             startPoint = endPoint
         return totalDistance
@@ -52,12 +66,8 @@ def greedyOrder(points, start):
     return greedy.greedyPathStart(points, start)
 
 
-def randomOrder(size, boardX, boardY):
-    ord = [[i, random.randint(0, boardX), random.randint(
-        0, boardY)] for i in range(1, size+1)]
-    # ord = []
-    # for i in range(1, size+1):
-    #     ord.append([i, random.randint(0, boardX), random.randint(0, boardY)])
+def randomOrder(points):
+    ord = deepcopy(points)
     random.shuffle(ord)
     ord.append(ord[0])
     return ord
@@ -68,13 +78,11 @@ def main():
 
     if (len(sys.argv) == 2):
         pointCount, points = instances.readFromFile(f'tests/{sys.argv[1]}')
-        boardX = max([point[1] for point in points])
-        boardY = max([point[2] for point in points])
     else:
-        pointCount, points = instances.randomInstance(
-            29, boardX := 2500, boardY := 2500)
+        pointCount, points = instances.randomInstance(29, 2500, 2500)
 
-    POPULATION_SIZE = 100
+    POPULATION_SIZE = 1000
+    s = int(POPULATION_SIZE/2)
     generation = 1
     population = []
 
@@ -82,21 +90,47 @@ def main():
         population.append(Individual(greedyOrder(points, i)))
 
     for _ in range(POPULATION_SIZE-pointCount):
-        population.append(Individual(randomOrder(pointCount, boardX, boardY)))
+        population.append(Individual(randomOrder(points)))
+
+    # for _ in range(POPULATION_SIZE):
+    #     population.append(Individual(randomOrder(points)))
 
     population.sort(key=lambda x: x.distance)
 
-    # for ex in population:
-    #     print([x[0] for x in ex.order], end=" ")
-    #     print(ex.distance)
+    print(f"Generation {generation}, distance {population[0].distance}, length {len(population[0].order)}")
 
-    for _ in range(100):
+    for _ in range(1000):
+        generation += 1
         newPopulation = []
         best = int((10*POPULATION_SIZE)/100)
         newPopulation.extend(population[:best])
+        # print("\n\nBest")
+        # print([int(x.distance) for x in newPopulation])
+
+        mate = int((80*POPULATION_SIZE)/100)
+        for _ in range(mate):
+            parent1 = random.choice(population[:s])
+            parent2 = random.choice(population[:s])
+            child = Individual.mate(parent1.order, parent2.order)
+            newPopulation.append(Individual(child))
+        # print("\nAfter mate")
+        # print([int(x.distance) for x in newPopulation])
+        
+        mut = int((10*POPULATION_SIZE)/100)
+        for _ in range(mut):
+            x = deepcopy(random.choice(population[:s]))
+            x.mutate()
+            x.distance = x.calDistance()
+            newPopulation.append(x)
+        # print("\nAfter mut")
+        # print([int(x.distance) for x in newPopulation])
 
         population = newPopulation
-        generation += 1
+        population.sort(key=lambda x: x.distance)
+        # print("\nEnd")
+        # print([int(x.distance) for x in population])
+
+        print(f"Generation {generation}, distance {population[0].distance}")
 
 
 if __name__ == "__main__":
--- instances.py
+++ instances.py
@@ -11,10 +11,11 @@ def randomInstance(instanceSize, boardX, boardY):
     points = []
     x = [int(x) for x in range(boardX-1)]
     y = [int(x) for x in range(boardY-1)]
+    pointSet = set()
     i = 1
     while len(points) != instanceSize:
-        point = [i, random.choice(x), random.choice(y)]
-        if point not in points:
-            points.append(point)
+        point = (random.choice(x), random.choice(y))
+        if point not in pointSet:
+            points.append([i, point[0], point[1]])
             i += 1
     return instanceSize, points
